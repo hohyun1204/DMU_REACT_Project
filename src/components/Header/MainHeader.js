afeccde9fda201d69791ef7ded00404d689e0c78
hohyun <hohyun1204@naver.com>
2023-05-28 00:14:33 +0900

    first commit

--- src/components/Header/MainHeader.js
+++ src/components/Header/MainHeader.js
@@ -4,10 +4,12 @@ import logo from 'assets/images/logo.png'
 function MainHeader(){
     return(
         <div class="header">
-            <img class="logo_img" src={logo}></img>
-            <div class="logo_title">
-                <span class="name">강의어때</span>
-                <span class="sub_name">동양미래대</span>
+            <div class="logo">
+                <img class="logo_img" src={logo}></img>
+                <div class="logo_title">
+                    <span class="name">강의어때</span>
+                    <span class="sub_name">동양미래대</span>
+                </div>
             </div>
         </div>
     );
